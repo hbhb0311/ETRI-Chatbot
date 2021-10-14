903b94567b229d290d436fa74d3aac1834004364
hbhb0311 <gpqls9712@gmail.com>
2021-10-14 19:19:31 +0900

    cdQA 테스트

--- cdqatest.py
+++ cdqatest.py
@@ -4,16 +4,16 @@ from ast import literal_eval
 from cdqa.utils.filters import filter_paragraphs
 from cdqa.pipeline import QAPipeline
 from cdqa.utils.download import download_model, download_bnpp_data
-#
-# download_bnpp_data(dir='./data/bnpp_newsroom_v1.1/')
-# download_model(model='bert-squad_1.1', dir='./models')
 
-df = pd.read_csv('./bnpp_newsroom-v1.1.csv', converters={'paragraphs': literal_eval})
+# download_bnpp_data(dir='./data/bnpp_newsroom_v1.1/')
+download_model(model='bert-squad_1.1', dir='./models')
+
+df = pd.read_csv('/Users/hyebin/Desktop/github/KB/CSV/loanFinal.csv', converters={'paragraphs': literal_eval})
 df = filter_paragraphs(df)
 
-cdqa_pipeline = QAPipeline(reader='data/bert_qa.joblib')
+cdqa_pipeline = QAPipeline(reader='./models/bert_qa.joblib')
 cdqa_pipeline.fit_retriever(df=df)
 
-query = 'non-voting director'
+query = 'KB 공무원우대대출에 대해 설명해줘'
 prediction = cdqa_pipeline.predict(query)
 print(prediction[0])
